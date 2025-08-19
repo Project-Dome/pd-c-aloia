ed35d0e355fde29301710fee5585eb2e9a1108f3
Lucas Monaco <lucas.monaco@projectdome.com.br>
2025-08-19 11:40:44 -0300

    auto commit

--- src/FileCabinet/SuiteScripts/pd_commission_report/pd_crc_static/pd-ccr-screen.js
+++ src/FileCabinet/SuiteScripts/pd_commission_report/pd_crc_static/pd-ccr-screen.js
@@ -22,6 +22,8 @@ function loadPartnerTable(options) {
     var _listBody = $('#commission-table');
     var _listBodyContents = '';
 
+    let lucas = 'lucas';
+
     _listBody.children().remove();
 
     _listBodyContents += `
