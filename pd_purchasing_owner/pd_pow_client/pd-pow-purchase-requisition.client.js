c6c538103176ab0270ecf87ae7dfe7fafd7d8568
PDRogrio2202 <rogerio.rodrigues@projectdome.com.br>
2025-09-10 18:26:56 -0300

    Inserting struture the new development.

---
+++ pd_purchasing_owner/pd_pow_client/pd-pow-purchase-requisition.client.js
@@ -0,0 +1,11 @@
+/**
+ * @NApiVersion     2.1
+ * @NScriptType     ClientScript
+ * @NModuleScope    SameAccount
+ * @author          Project Dome - Rogério Gonçalves Rodrigues
+ */
+
+define([], function(){
+
+    return{}
+});
